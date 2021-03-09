94133693357f15114812db421944fb47cd0d6b5e
오경환 <okyeonghwan5@gmail.com>
2021-03-09 11:29:20 +0900

    hash set ex

--- JavaEx/src/com/javaex/collection/hash/HashSetEx.java
+++ JavaEx/src/com/javaex/collection/hash/HashSetEx.java
@@ -6,8 +6,37 @@ public class HashSetEx {
 
 	public static void main(String[] args) {
 
-		basicHashSetEx();
+		//basicHashSetEx();
+		customHashSetEx();
 	
+	}
+	
+	private static void customHashSetEx() {
+		
+		HashSet<Student> hs = new HashSet<>();
+		
+		Student s1 = new Student("Hong", 10);
+		Student s2 = new Student("Jang", 20);
+		Student s3 = new Student("Jeon", 30);
+		
+		hs.add(s1);
+		hs.add(s2);
+		hs.add(s3);
+		
+		System.out.println("학생부:" + hs);
+		
+		Student s4 = new Student("Hong", 10);	//같은값을 가진다 하더라도 추가됨 
+		hs.add(s4);
+		System.out.println("hashcode of s1 " + s1.hashCode());	//s1 memory addr
+		System.out.println("hashcode of s4 " + s4.hashCode());	//s4 momory addr
+		System.out.println("s1 equals s4 ? " + s1.equals(s4));
+		
+		System.out.println("학생부:" + hs);
+		
+		// 포함 여부 확인
+		boolean exists = hs.contains(new Student("Hong", 10));
+		System.out.println("Hong(10) exists:" + exists);
+		
 	}
 	
 	private static void basicHashSetEx() {
